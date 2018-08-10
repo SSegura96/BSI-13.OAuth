9da3030958253b66636e2d2c78958e77b7a1da8a
Sergio Luis Segura Vidal <sergio31.96@outlook.com>
2018-08-09 22:58:06 -0600

    Adding base project

---
+++ BSI-13.OAuth/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BSI_13.OAuth.MvcApplication" Language="C#" %>
---
+++ BSI-13.OAuth/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BSI_13.OAuth.Startup))]
+namespace BSI_13.OAuth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
